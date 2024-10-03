3ef7b3eb8895ce754d9a3316f15fe5ea77ee551c
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-10-03 21:19:57 +0500

    feat: toggle light and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { SignUp } from "./components/SignUp/signUp";
+import { ThemeProvider } from "./components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <div className="h-full flex">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/register" element={<SignUp />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider>
+        <ModeToggle className="absolute top-2 right-10 "/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<SignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
